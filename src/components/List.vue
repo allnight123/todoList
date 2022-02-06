9ce52357adeb4c079a6613f2a075b388e69d64a2
Hecle <allnight123@naver.com>
2022-02-07 04:18:17 +0900

    List.vue 내용 추가
    카드 형태 박스 추가
    
    for 문으로 반복시킴
    
            <v-card
                class="pa-3"
                    v-for="(list, index) in todoList"
                    :key="index"
            >
                <p>{{ list.memo }}  </p>
                <v-btn fab flat small color=green><i class="fas fa-check"></i></v-btn>
                <v-btn fab flat small color=blue><i class="fas fa-redo-alt"></i></v-btn>
                <v-btn fab flat small color=red><i class="fas fa-trash-alt"></i></v-btn>
            </v-card>
    
    데이터는 props 로 가지고 온다.
    
    <script>
    export default {
        props : ['todoList'],
        data() {
            return {
    
            }
        },
    }
    </script>

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        투두 리스트
+        <v-card 
+            class="pa-3"
+                v-for="(list, index) in todoList"
+                :key="index"
+        >
+            <p>{{ list.memo }}  </p>
+            <v-btn fab flat small color=green><i class="fas fa-check"></i></v-btn>
+            <v-btn fab flat small color=blue><i class="fas fa-redo-alt"></i></v-btn>
+            <v-btn fab flat small color=red><i class="fas fa-trash-alt"></i></v-btn>
+        </v-card>        
     </div>
 </template>
+
+<script>
+export default {
+    props : ['todoList'],
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
