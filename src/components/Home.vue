8717fe9bf81fbdbf4aa3b900f3611e8d07ca33a0
Hecle <allnight123@naver.com>
2022-02-07 02:07:49 +0900

    Home.vue 부품파일 2개 추가
    import List from "./List"
    import ListAdd from "./ListAdd"
    
    export default {
      components: {
        List,
        ListAdd
      },

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,35 @@
 <template>
-<v-container>
-</v-container>
+  <v-container>
+    {{ todoList }}
+    <v-layout row wrap>
+      <v-flex xs6>
+        <List/>
+      </v-flex>
+      <v-flex xs6>
+        <ListAdd
+          @listAdd = 'listAdd'
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import List from "./List"
+import ListAdd from "./ListAdd"
+
 export default {
+  components: {
+    List,
+    ListAdd
+  },
+
   data() {
     return {
       todoList: []
     }
   },
+
   methods: {
     listAdd(memo) {
       console.log("받았어!")
