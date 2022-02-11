e0fe034ff45be50b9cf1e07238c09921f202c173
Hecle <allnight123@naver.com>
2022-02-11 13:51:18 +0900

    todo list 완성본

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <v-container>
     <v-layout row wrap>
+      <v-flex xs12 text-xs-center>
+        <h1>투두 리스트</h1>
+        <p>
+          전체할일 : {{ todoList.length }} / 완료된 할일: {{ countDone }} / 남은
+          할일: {{ todoList.length - countDone }}
+        </p>
+      </v-flex>
       <v-flex xs6 pa-2>
-        <List 
-          :todoList='todoList'
+        <List
+          :todoList="todoList"
+          @statusControl="statusControl"
+          @listDelete="listDelete"
         />
       </v-flex>
       <v-flex xs6 pa-2>
-        <ListAdd
-          @listAdd = 'listAdd'
+        <ListAdd 
+          @listAdd="listAdd" 
+          @listEdit="listEdit" 
         />
       </v-flex>
     </v-layout>
@@ -16,26 +26,52 @@
 </template>
 
 <script>
-import List from "./List"
-import ListAdd from "./ListAdd"
+import List from "./List";
+import ListAdd from "./ListAdd";
 
 export default {
   components: {
     List,
-    ListAdd
+    ListAdd,
   },
 
   data() {
     return {
-      todoList: []
-    }
+      todoList: [],
+    };
+  },
+  computed: {
+    countDone() {
+      let count = 0;
+      this.todoList.forEach((list) => {
+        if (list.status === "done") count++;
+      });
+      return count;
+    },
   },
 
   methods: {
     listAdd(memo) {
-      console.log("받았어!")
-      this.todoList.push({ memo: memo, status: "created" })
-    }
-  }
-}
+      console.log("받았어!");
+      this.todoList.push({ memo: memo, status: "created" });
+    },
+
+    listEdit(memo, index) {
+      console.log("수정할거 받았어!");
+      this.todoList[index].memo = memo;
+    },
+
+    statusControl(index, status) {
+      console.log(index, status);
+      this.todoList[index].status = status;
+    },
+
+    listDelete(index) {
+      this.todoList.splice(index, 1);
+      //                   번호,갯수
+      //            splice는 지우는 명령어
+      //                        갯수의 숫자가 2 라면 인덱스 번호로 부터 2개를 삭제한다.
+    },
+  },
+};
 </script>
--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -6,16 +6,27 @@
       label="투두리스트를 입력해주세요"
       value=""
     ></v-textarea>
-    <v-btn @click="listAdd">리스트 추가</v-btn>
+    <v-btn v-if="mode === 'add'"    @click="listAdd">리스트 추가</v-btn>
+    <v-btn v-else   @click="listEdit">리스트 수정</v-btn>
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
 export default {
   data() {
     return {
       memo: null,
+      index: null,
+      mode: 'add'
     };
+  },
+  created() {
+      eventBus.$on('listEdit', (memo , index) =>{
+          this.memo = memo
+          this.index = index
+          this.mode = 'edit'
+      })
   },
   methods: {
     listAdd() {
@@ -24,8 +35,16 @@ export default {
             alert('할일을 입력해주세요')
         }else {
             this.$emit('listAdd' , this.memo)
-        }
+            this.memo = null
+        }        
     },
+
+    listEdit() {
+        console.log ('리스트 수정')
+            this.$emit('listEdit' , this.memo , this.index)
+            this.memo = null
+            this.mode = 'add'
+    }
   },
 };
 </script>
